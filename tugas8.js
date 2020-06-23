85df5145524f8d33f1a0f986143d920e9b293d5c
maitasawilia <maitasaw@gmail.com>
2020-06-23 22:46:28 +0700

    Tugas 8 JS
    Tugas ini dikumpulkan untuk Niomic

---
+++ tugas8.js
@@ -0,0 +1,13 @@
+function panggilObjects() {
+  var makanan = {
+    type: 'Kering',
+    harga: 15000,
+    rasa: 'Pedas',
+  }
+
+  makanan.rasa = 'Manis'
+  console.log(makanan)
+  console.log(makanan.harga)
+}
+
+panggilObjects()
